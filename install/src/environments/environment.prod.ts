86663c452b28d83d48962da454c9695035d47859
Catalin MOLDOVAN <catalin.moldovan@ro.ibm.com>
2022-11-01 21:53:24 +0200

    S

--- install/src/environments/environment.prod.ts
+++ install/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backend: 'http://mylb-868721875.eu-central-1.elb.amazonaws.com:1101', // Put your backend here
-  assistantApp: 'http://mylb-868721875.eu-central-1.elb.amazonaws.com:2001',
-  backendImmoReport: 'http://mylb-868721875.eu-central-1.elb.amazonaws.com:1401',
+  backendImmoReport: 'https://app.itwiththebest.com:1402',
+  backend: 'https://app.itwiththebest.com:1102', // Put your backend here
+  assistantApp: 'https://myimmo-frontend.itwiththebest.com',
 };
